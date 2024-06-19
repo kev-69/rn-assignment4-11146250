b3ed25b61c3e0a110a0701df58659a5a9aefe731
kev-69 <bismarkobuobi19@gmail.com>
2024-06-19 22:59:28 +0000

    Created files for mockup datta

---
+++ assignment-4/mockups/featuredjobs.jsx
@@ -0,0 +1,22 @@
+const featuredJobs = [
+    {
+        id: 0,
+        backgroundColor : '#1679AB',
+        logo : require("../assets/facebook.png"),
+        role: 'Software Engineer',
+        company : 'facebook',
+        salary: '$180,00',
+        location: 'Accra,Ghana'
+    },
+    {
+        id: 1,
+        backgroundColor : '#102C57',
+        logo : require('../assets/google.png'),
+        role: 'Front-End Developer',
+        company : 'Google',
+        salary: '$195,00',
+        location: 'Kumasi,Ghana'
+    }
+]
+
+export default featuredJobs;
---
+++ assignment-4/mockups/popularjobs.jsx
@@ -0,0 +1,28 @@
+const popularJobs = [
+    {
+        id: 0,
+        logo : require('../assets/burger_king.png'),
+        role: 'Jr Executive',
+        company : 'Burger King',
+        salary: '$96,000/y',
+        location: 'Los Angels, US'
+    },
+    {
+        id: 1,
+        logo : require('../assets/beats.png'),
+        role: 'Product Manager',
+        company : 'Beats',
+        salary: '$84,000/y',
+        location: 'Florida, US'
+    },
+    {
+        id: 2,
+        logo : require('../assets/facbook1.png'),
+        role: 'Product Manager',
+        company : 'Facebook',
+        salary: '$86,000/y',
+        location: 'Florida, US'
+    }
+]
+
+export default popularJobs;
